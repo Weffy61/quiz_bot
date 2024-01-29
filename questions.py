caa60c1aad868054c63bbacdf0966f40fbeca94b
Weffy61 <weffy@mail.ru>
2024-01-29 22:12:12 +0300

    Rename var

--- questions.py
+++ questions.py
@@ -3,16 +3,16 @@ def get_questions(file_path):
     with open(file_path, 'r', encoding='KOI8-R') as file:
         file_content = file.read()
 
-    split_file = file_content.split('\n\n')
+    split_content = file_content.split('\n\n')
     questions = {}
 
-    for question_number in range(len(split_file))[3:]:
-        if 'Вопрос' in split_file[question_number]:
-            question_title, question = split_file[question_number].split(':\n')
-            answer_title, answer = split_file[question_number + 1].split(':\n')
+    for question_number in range(len(split_content))[3:]:
+        if 'Вопрос' in split_content[question_number]:
+            question_title, question = split_content[question_number].split(':\n')
+            answer_title, answer = split_content[question_number + 1].split(':\n')
             questions[question] = answer
-        elif 'Автор' in split_file[question_number] or 'Источник' in split_file[question_number] or 'Ответ' in \
-                split_file[question_number]:
+        elif 'Автор' in split_content[question_number] or 'Источник' in split_content[question_number] or 'Ответ' in \
+                split_content[question_number]:
             continue
     return questions
 
